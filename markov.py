c1574e1ecaa7dc3698f67bdfab3b3bb9083ded8b
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-17 12:53:01 -0700

    solved for capitalization and multiple files

--- markov.py
+++ markov.py
@@ -4,64 +4,27 @@ from random import choice
 import sys
 
 
-def open_and_read_file(file_path):
+def open_and_read_file(file_paths):
     """Take file path as string; return text as string.
 
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
 
-    the_file = open(file_path)
-    file_text = the_file.read()
+    combined_text = ""
 
-    words = file_text.split()
+    files = file_paths[1:]
 
-    the_file.close()
+    for poem in files:
+        the_file = open(poem)
+        file_text = the_file.read()
+
+        combined_text += file_text
+        the_file.close()
+
+    words = combined_text.split()
 
     return words
-
-
-# def make_chains(words):
-#     """Take input text as string; return dictionary of Markov chains.
-
-#     A chain will be a key that consists of a tuple of (word1, word2)
-#     and the value would be a list of the word(s) that follow those two
-#     words in the input text.
-
-#     For example:
-
-#         >>> chains = make_chains("hi there mary hi there juanita")
-
-#     Each bigram (except the last) will be a key in chains:
-
-#         >>> sorted(chains.keys())
-#         [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
-#     Each item in chains is a list of all possible following words:
-
-#         >>> chains[('hi', 'there')]
-#         ['mary', 'juanita']
-        
-#         >>> chains[('there','juanita')]
-#         [None]
-#     """
-
-#     chains = {}
-
-
-#     # loop through list of words, create tuples of bigrams
-#     # see if tuple is already in dictionary, if not add to dictionary, else append next word
-#     for i, word in enumerate(words):
-#         if i < len(words) - 2:
-#             bigram_tuple = (word, words[i + 1])
-            
-#             if chains.get(bigram_tuple) == None:
-#                 chains[bigram_tuple] = [words[i + 2]]
-
-#             else:
-#                 chains[bigram_tuple].append(words[i + 2])
-
-#     return chains
 
 
 def make_chains(words, n_words):
@@ -92,8 +55,12 @@ def make_text(chains, n_words):
 
     words = []
 
-    # if you want to start with random tuple:    
-    tuple_in_use = choice(list(chains))
+    # if you want to start with random tuple: 
+    while True:
+        tuple_in_use = choice(list(chains))
+
+        if tuple_in_use[0][0].isupper():
+            break
 
     words.extend(tuple_in_use)
 
@@ -114,7 +81,8 @@ def make_text(chains, n_words):
     return " ".join(words)
 
 # Open the file and turn it into one long string
-input_text = open_and_read_file(sys.argv[1])
+# input_text = open_and_read_file(sys.argv[1], sys.argv[2])
+input_text = open_and_read_file(sys.argv)
 
 n_gram = int(input("How many words in your n-gram? "))
 
